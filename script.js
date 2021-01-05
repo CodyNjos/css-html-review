1c961ae4ca74d9512595df8e6d33e39fc17eddd7
Cody Njos <cody.njos@live.com>
2021-01-04 18:51:22 -0600

    made basic page, added mouseenter and mouseleave for inputs

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
-
 $( document ).ready(readyNow);
 function readyNow() {
-    console.log('jQuerry & js linked')
+    console.log('jQuerry & js linked');
+    $( 'input' ).mouseenter( whiteHighlight );
+    $( 'input' ).mouseleave( noHighlight);
+    $( '#submitPerson').on( 'click' , submit2);
+
+}
+
+
+
+let person = []
+
+
+function whiteHighlight() {
+    $( this ).css('border-color' , 'white');
+}
+function noHighlight() {
+    $( this ).css('border-color' , '')
+}
+function submit(firstName, middleName , lastName) {
+   
+    const personSubmit = {
+        first : firstName,
+        middle : middleName ,
+        last : lastName,
+    }
+    person.push(personSubmit);
+    console.log(person);
+    return true;
+}
     
+
+function submit2() {
+    let firstName2 = $( '#firstName' ).val();
+    let middleName2 = $( '#middleName' ).val();
+    let lastName2 = $( '#lastName' ).val();
+    let list = $('#list')
+   
+    submit(firstName2 , middleName2 , lastName2)
+    list.append( '<li>' + firstName2 + ' ' + middleName2 + ' ' + lastName2 + '</li>');
+    $( '#firstName' ).val('');
+    $( '#middleName' ).val('');
+    $( '#lastName').val('');
 }
